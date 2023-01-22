2aef9402c6408d40f7a332f6b5cedd9c7fb66880
Galkin Dmitry <dimitriusgalkin@gmail.com>
2023-01-22 15:46:44 +0300

    Task1_factorial

--- Seminar1/task_1.java
+++ Seminar1/task_1.java
@@ -5,16 +5,25 @@ package Seminar1;
 
 import java.util.Scanner;
 
- public class task_1 {
+public class task_1 {
+
     public static void main(String[] args) {
-        int sumDigit=0;
+        int sumDigit = 0;
+        int factDigit = 1;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите число: ");
         int digit = scanner.nextInt();
         for (int i = 1; i <= digit; i++) {
             sumDigit += i;
         }
-        System.out.println( sumDigit);
+        for (int j = 1; j <= digit; j++) {
+            if (digit == 0) {
+                factDigit = 1;
+            }
+            factDigit *= j;
+        }
+        System.out.printf("Cумма чисел от 1 до %d равна %d, Факториал числа %d равен %d",
+                digit, sumDigit, digit, factDigit);
         scanner.close();
     }
 }
